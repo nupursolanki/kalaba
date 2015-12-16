e227e726d54e832f58ee23aec074627d7b3a5839
nupursolanki <nupursolanki.d@gmail.com>
2015-12-17 00:02:45 +0530

    Term And condition Page

---
+++ wp-content/themes/ShipMe/page-templates/termandcondition.php
@@ -0,0 +1,28 @@
+<?php
+/*
+  Template Name: Term And Condition Page
+ */
+
+get_header();
+?>
+<div class="container_ship_ttl_wrap">	
+    <div class="container_ship_ttl">
+        <div class="my-page-title col-xs-12 col-sm-12 col-lg-12">
+            <?php the_title(); ?>
+        </div>
+    </div>
+</div>
+<div class="container_ship_no_bk mrg_topo">
+    <div class="job-content-area col-xs-12 col-sm-12 col-lg-12">
+        <ul class="virtual_sidebar" id="six-years">
+            <li class="widget-container widget_text">
+                <div class="my-only-widget-content termpageliststyle">
+                    <?php if (have_posts()) while (have_posts()) : the_post(); ?>
+                            <?php the_content(); ?>						
+                        <?php endwhile; // end of the loop. ?>
+                </div>
+            </li>
+        </ul>
+    </div>
+</div>
+<?php get_footer() ?>
